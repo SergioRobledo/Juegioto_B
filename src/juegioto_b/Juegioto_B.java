2922f4c7c29155fcea2cc0a716d7fdbb40f479c6
SergioRobledo <38915550+SergioRobledo@users.noreply.github.com>
2018-06-13 10:46:27 -0300

    Update and rename Juegioto_B.java to Pet_Fight.java

--- src/juegioto_b/Juegioto_B.java
+++
@@ -1,8 +0,0 @@
-package juegioto_b;
-	
-public class Juegioto_B {
-        
-    public static void main(String[] args) {
-        Menu.menu ();
-    }
-}
---
+++ src/juegioto_b/Pet_Fight.java
@@ -0,0 +1,8 @@
+package Pet_Fight;
+
+public class Pet_Fight {
+
+    public static void main(String[] args) {
+        Menu.menu();
+    }
+}
